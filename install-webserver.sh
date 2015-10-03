2c8563a4bb5b9f8d087b29804de6cf0e5aaf80bd
snehakarunakaran <skaruna1@hawk.iit.edu>
2015-10-03 02:38:50 -0500

    test issues

--- install-webserver.sh
+++ install-webserver.sh
@@ -7,6 +7,7 @@ git clone https://github.com/snehakarunakaran/ITMO544-Fall-Application.git
 
 mv ./ITMO544-Fall-Application/images /var/www/html/images
 mv ./ITMO544-Fall-Application/index.html /var/www/html
+mv ./ITMO544-Fall-Application/page2.html /var/www/html
 
 echo "Success" > /tmp/hello.txt
 
