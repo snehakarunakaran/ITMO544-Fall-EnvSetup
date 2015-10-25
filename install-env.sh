c9a6a369acb953eb289dd936df2a0c8aad9e9651
snehakarunakaran <skaruna1@hawk.iit.edu>
2015-10-25 10:21:21 -0500

    Reverted back the changes to install only apache and git and move the files

--- install-env.sh
+++ install-env.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 sudo apt-get -y update 
-sudo apt-get install -y apache2 git php5 php5-mysql php5-curl mysql-client curl
-sudo curl -sS https://getcomposer.org/installer | php
-sudo php composer.phar require aws/aws-sdk-php
+sudo apt-get install -y apache2 git
 
-
-sudo git clone https://github.com/snehakarunakaran/ITMO544-Fall-Application.git
+git clone https://github.com/snehakarunakaran/ITMO544-Fall-Application.git
 
 mv ./ITMO544-Fall-Application/images /var/www/html/images
 mv ./ITMO544-Fall-Application/index.html /var/www/html
