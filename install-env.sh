a77cb47c6a27053bc347392a4f63fa8ffcd3cf78
snehakarunakaran <skaruna1@hawk.iit.edu>
2015-10-25 00:37:29 -0500

    Updated the install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 sudo apt-get -y update 
-sudo apt-get install -y apache2 git
+sudo apt-get install -y apache2 git php5 php5-mysql php5-curl mysql-client curl
+sudo curl -sS https://getcomposer.org/installer | php
+sudo php composer.phar require aws/aws-sdk-php
 
-git clone https://github.com/snehakarunakaran/ITMO544-Fall-Application.git
+
+sudo git clone https://github.com/snehakarunakaran/ITMO544-Fall-Application.git
 
 mv ./ITMO544-Fall-Application/images /var/www/html/images
 mv ./ITMO544-Fall-Application/index.html /var/www/html
